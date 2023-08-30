78cd3b8b355d01e205b78a4af5ab9a13a75005eb
hveidsten <hveidsten@gmail.com>
2023-08-30 11:26:24 +0200

    Editing in cells

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { MaterialReactTable } from "material-react-table";
+import { Autocomplete, TextField } from "@mui/material";
+import _ from "lodash";
 
 const App = () => {
   const [transactions, setTransactions] = useState([]);
+  const [currencies, setCurrencies] = useState([]);
+
   useEffect(() => {
     const getTransactions = async () => {
       const data = await fetch(
@@ -14,6 +18,19 @@ const App = () => {
     };
     getTransactions();
   }, []);
+
+  useEffect(() => {
+    const uniqueCurrencies = _.uniqBy(transactions, "currency").map(
+      (obj) => obj.currency
+    );
+    setCurrencies(uniqueCurrencies);
+  }, [transactions]);
+
+  const handleSaveCell = (cell, value) => {
+    console.log(cell, value);
+    transactions[cell.row.index][cell.column.id] = value;
+    setTransactions([...transactions]);
+  };
 
   const columns = [
     {
@@ -31,12 +48,42 @@ const App = () => {
     {
       accessorKey: "currency",
       header: "Currency",
+      Edit: ({ cell, column, table }) => (
+        <Autocomplete
+          options={currencies}
+          value={cell.value}
+          renderInput={(params) => <TextField size="small" {...params} />}
+          onChange={(event, value) => handleSaveCell(cell, value)}
+        />
+      ),
+    },
+    {
+      accessorKey: "date",
+      header: "Date",
+    },
+    {
+      accessorKey: "invoiceDueDate",
+      header: "Due Date",
+    },
+    {
+      accessorKey: "id",
+      header: "ID",
     },
   ];
 
   return (
     <div className="App">
-      <MaterialReactTable columns={columns} data={transactions} />
+      <MaterialReactTable
+        editingMode="cell"
+        enableEditing
+        muiTableBodyCellEditTextFieldProps={({ cell }) => ({
+          onBlur: (event) => {
+            handleSaveCell(cell, event.target.value);
+          },
+        })}
+        columns={columns}
+        data={transactions}
+      />
     </div>
   );
 };
